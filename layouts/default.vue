8d793c996209cb70e93b51d6f9eb646afd428fb5
hydyrayhan <hydyrowayhan7@gmail.com>
2022-05-12 14:48:22 +0500

    new computer

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <Header />
+        <Nuxt />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+// import { mapGetters, mapActions } from 'vuex';
+export default {
+    components: {Header, Footer},
+    created() {
+        // this.$store.dispatch('cart/setProductsToCart')
+        // const userToken = this.$store.getters['user/userToken']
+        // if (userToken) this.$axios.setHeader('Authorization', `Bearer ${userToken}`)
+    },
+    mounted() {
+        // let cartPerfumes = localStorage.getItem('cart-products')
+        // if (!cartPerfumes) cartPerfumes = []
+        // else cartPerfumes = JSON.parse(cartPerfumes)
+        // this.$store.dispatch('cart/setProductsToCart', cartPerfumes)
+    },
+    computed:{
+        // ...mapGetters({
+        //     // search: 'search/search',
+        // }),
+    },
+}
+</script>
