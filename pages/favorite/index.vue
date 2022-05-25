0dd8953a41b6bb02612ec5830e3b966e0c915942
hydyrayhan <hydyrowayhan7@gmail.com>
2022-05-25 17:26:41 +0500

    favorite

---
+++ pages/favorite/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="favorite_head">{{$t('liked')}}</div>
+
+    <div class="favorite_products">
+      <LongProduct v-for="i in 15" :key="i"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import LongProduct from '~/components/LongProduct';
+export default {
+  components:{LongProduct},
+  mounted(){
+    console.log(document.querySelector(".header_buttons_like path").style.fill = 'red')
+  }
+}
+</script>
+
+<style>
+
+</style>
